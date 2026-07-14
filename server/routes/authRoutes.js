de46b06c9469d31b8c666575c77580eafd123393
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-14 20:44:11 +0300

    Adding login route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
-import { register } from "../controllers/authControllers.js"
+import { login, register } from "../controllers/authControllers.js"
 import express from 'express'
 
 const authRoute = express.Router()
 authRoute.post('/register',register)
+authRoute.post('/login',login)
 export default authRoute  
