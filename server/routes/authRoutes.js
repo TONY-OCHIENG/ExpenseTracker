1dc0c6fefd41755e331b98148a7eabc9a3bafb37
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-15 09:54:15 +0300

    Adding middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import { login, register, userDetails } from "../controllers/authControllers.js"
 import express from 'express'
+import { auth } from "../middlewares/auth.js"
 
 const authRoute = express.Router()
 authRoute.post('/register',register)
 authRoute.post('/login',login)
-authRoute.get('/user',userDetails)
+authRoute.get('/user',auth,userDetails)
 export default authRoute  
