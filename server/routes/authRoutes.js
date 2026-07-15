8689f5285e8f53ef3ca5f63ca51dc5245caefcd2
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-15 09:52:31 +0300

    Adding user details toute

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,8 @@
-import { login, register } from "../controllers/authControllers.js"
+import { login, register, userDetails } from "../controllers/authControllers.js"
 import express from 'express'
 
 const authRoute = express.Router()
 authRoute.post('/register',register)
 authRoute.post('/login',login)
+authRoute.get('/user',userDetails)
 export default authRoute  
