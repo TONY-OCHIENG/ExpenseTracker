87bde9858673f6590f074e19e71b53bd608eca0d
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-15 10:01:36 +0300

    Adding cookie-parser

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import authRoute from './routes/authRoutes.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors({
     origin:'http://localhost:5173',
     methods:['PUT','GET','POST','DELETE'],
