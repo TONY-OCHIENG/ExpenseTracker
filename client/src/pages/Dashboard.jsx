a24e67e4dffb9e978ff6a12fd804458f7a755f3d
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-15 10:06:20 +0300

    Setting user authentication

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
+  const [auth,setAuth] = useState(false)
+  const [details,setDetails] = useState([])
+  const navigate = useNavigate()
+  axios.defaults.withCredentials = true
+  useEffect(() => {
+    axios.get('http://localhost:3000/auth/user')
+    .then((response) => {
+      if (response.data.status) {
+        setDetails(response.data.details)
+        setAuth(true)
+      } else {
+        navigate('/login')
+        setAuth(false)
+      }
+    })
+    .catch((error) => {console.log(error)})
+  },[])
   return (
     <div>Dashboard</div>
   )
