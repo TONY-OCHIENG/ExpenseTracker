77674316f018235df2817c4890fd877b1674b152
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-16 16:31:20 +0300

    Refactoring Dashboard navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Router, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
 import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Income from './pages/Income'
+import Expense from './pages/Expense'
 
 function App() {
   return (
@@ -10,7 +13,11 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}>
+        <Route path='' element={<Home/>}/>
+        <Route path='/dashboard/income' element={<Income/>}/>
+        <Route path='/dashboard/expense' element={<Expense/>}/>
+        </Route>
       </Routes>
     </div>
   )
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom'
-import { BanknoteArrowDown, LayoutDashboard, TrendingUp } from 'lucide-react'
+import { BanknoteArrowDown, LayoutDashboard, TrendingUp, UserIcon } from 'lucide-react'
 function Dashboard() {
   const [auth,setAuth] = useState(false)
   const [details,setDetails] = useState([])
@@ -23,22 +23,23 @@ function Dashboard() {
   },[])
   const navLinks = [
     {name:"Dashboard", link:"/dashboard",icon: LayoutDashboard},
-    {name:"Income", link: "/income", icon: TrendingUp},
-    {name:"Expense", link: "/expense", icon: BanknoteArrowDown}
+    {name:"Income", link: "/dashboard/income", icon: TrendingUp},
+    {name:"Expense", link: "/dashboard/expense", icon: BanknoteArrowDown}
   ]
   return (    
-    <div>{
-      auth ? <div className='w-full h-[100vh] flex bg-gray-100'>
-       <div className={`fixed left-0 bottom-0 h-[100vh] md:w-[15%] w-[25%] border bg-white p-2`}>
+    <div className='w-full'>{
+      auth && 
+      <div className='w-full h-[100vh] flex bg-gray-100'>
+       <div className={`h-[100vh] md:w-[15%] w-[25%] border bg-white p-2`}>
         <div className='h-15 w-15 ml-5 md:ml-15 md:h-20 md:w-20 border mt-10 rounded-full bg-gradient-to-tl from-blue-900 via-blue-500 to-blue-200 flex items-center justify-center'>
          <h1 className='text-white font-extrabold text-4xl uppercase'> {details.firstName.charAt(0) + details.lastName.charAt(0)}</h1>
         </div>
-         <p className='md:ml-15 mt-2 text-sm font-bold text-gray-600'> {details.firstName +" "+ details.lastName}</p>
+         <p className='md:ml-15 mt-2 text-sm font-bold text-gray-600 mb-10'> {details.firstName +" "+ details.lastName}</p>
            {
                       navLinks.map((link,index) => (
                           <NavLink key={index} to={link.link} end className={({isActive}) => `relative flex items-center
                               max-md:justify-center gap-2 w-full rounded-2 py-2.5 min-md:pl-10 first:mt-6
-                              text-gray-900 ${isActive && 'bg-blue-600 text-white group mt-10 rounded-md'}`}>
+                              text-gray-900 ${isActive && 'bg-blue-600 text-white group  rounded-md'}`}>
                               {({isActive}) => (
                                   <>
                                     <link.icon className={`w-5 h-5 text-blue-600 ${isActive && 'text-white'}`}/>
@@ -49,10 +50,10 @@ function Dashboard() {
                       ))
             }
        </div>
-       <div>
+      <div className='flex overflow-y-auto flex-col'>
         <Outlet/>
-       </div>
-      </div> : <>Session expired <Link to={'/login'} className='font-extrabold'>Click to login</Link></>      
+      </div>
+      </div>    
     }</div>
   )
 }
