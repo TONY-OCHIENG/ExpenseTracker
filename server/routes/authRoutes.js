231ac63c660e6dc0ce4ad284059fd2e756363b76
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-17 09:16:01 +0300

    logout controller

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,4 +1,4 @@
-import { login, register, userDetails } from "../controllers/authControllers.js"
+import { login, logout, register, userDetails } from "../controllers/authControllers.js"
 import express from 'express'
 import { auth } from "../middlewares/auth.js"
 
@@ -6,4 +6,5 @@ const authRoute = express.Router()
 authRoute.post('/register',register)
 authRoute.post('/login',login)
 authRoute.get('/user',auth,userDetails)
+authRoute.get('/logout',logout)
 export default authRoute  
