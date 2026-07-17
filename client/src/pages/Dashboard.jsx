4229614d4d6837105bf03d3230ceaa528b122355
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-17 09:10:23 +0300

    implemnting a function that handles logout

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom'
-import { BanknoteArrowDown, LayoutDashboard, TrendingUp, UserIcon } from 'lucide-react'
+import { BanknoteArrowDown, LayoutDashboard, PowerCircle, PowerCircleIcon, TrendingUp, UserIcon } from 'lucide-react'
+import { toast }from 'react-toastify'
 function Dashboard() {
   const [auth,setAuth] = useState(false)
   const [details,setDetails] = useState([])
@@ -26,11 +27,22 @@ function Dashboard() {
     {name:"Income", link: "/dashboard/income", icon: TrendingUp},
     {name:"Expense", link: "/dashboard/expense", icon: BanknoteArrowDown}
   ]
+  const handleLogout = () => {
+    axios.get('http://localhost:3000/auth/logout')
+    .then((response) => {
+      if (response.data.status) {
+        navigate('/login')
+        toast.success(response.data.message)
+      } else {
+        toast.error("An error occured")
+      }
+    })
+  }
   return (    
     <div className='w-full'>{
       auth && 
       <div className='w-full h-[100vh] flex bg-gray-100'>
-       <div className={`relative h-[100vh] md:w-[15%] w-[25%] border bg-white p-2`}>
+       <div className={`relative h-[100vh] md:w-[15%] w-[25%] border-r bg-white p-2`}>
         <div className='h-15 w-15 ml-5 md:ml-15 md:h-20 md:w-20 border mt-10 rounded-full bg-gradient-to-tl from-blue-900 via-blue-500 to-blue-200 flex items-center justify-center'>
          <h1 className='text-white font-extrabold text-4xl uppercase'> {details.firstName.charAt(0) + details.lastName.charAt(0)}</h1>
         </div>
@@ -49,6 +61,9 @@ function Dashboard() {
                 </NavLink>
               ))
             }
+             <div className='min-md:pl-10 pl-7 text-gray-900 mt-2.5'>
+               <p className='flex gap-2 cursor-pointer' onClick={() => handleLogout()}><PowerCircle className='text-blue-600'/> <span className='max-md:hidden'>Logout</span></p>
+              </div>
        </div>
       <div className='flex overflow-y-auto flex-col w-[85%]'>
         <div className=' border-b bg-white p-4 w-full shadow-md'>
