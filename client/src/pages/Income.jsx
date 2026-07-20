07f73010108e914745c24df3fc75d64db46ca37f
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-20 14:01:15 +0300

    Fetching userId from the database

--- client/src/pages/Income.jsx
+++ client/src/pages/Income.jsx
@@ -1,14 +1,33 @@
+import axios from 'axios'
 import { Download, XIcon } from 'lucide-react'
 import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
 
 function Income() {
   const [open,setOpen] = useState(false)
+  const [details,setDetails] = useState({
+    userId: '',
+    incomeDetails:'',
+    incomePrice:'',
+    incomeDate:'',
+  })
+  useEffect(() => {
+    axios.get('http://localhost:3000/auth/user')
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => { console.log(error)})
+  },[])
   const handleOpen = () => {
     setOpen(!open)
   }
+  const handleDetails = (event) => {
+    const { name, value} = event.target
+  }
+  
   return (
-    <div className='relative w-full h-full'>
+    <div className={`relative w-full h-full ${open ? 'overflow-hidden' : ''}`}>
       <div className='max-w-7xl md:w-[90%] mx-auto px-2 w-full'>
         <div className='p-4 bg-white rounded-md mt-4 flex flex-col h-[400px] mb-2'>
           <div className='flex justify-between mb-2'>
@@ -16,7 +35,7 @@ function Income() {
                 <h1 className='text-md text-gray-800 font-extrabold'>Income Overview</h1>
                 <p className='text-xs text-gray-600'>Track your earnings over time and analyse your income trends</p>
             </div>
-            <button className='py-2 px-4 text-blue-600 font-extrabold cursor-pointer border rounded-md border-blue-600'>+ Add Income</button>
+            <button className='py-2 px-4 text-blue-600 font-extrabold cursor-pointer border rounded-md border-blue-600' onClick={() => handleOpen()}>+ Add Income</button>
           </div>
         </div>
            <div className='p-4 bg-white rounded-md mt-4 flex flex-col h-[400px] overflow-y-auto'>
@@ -29,10 +48,10 @@ function Income() {
         </div>
 
       </div>
-      <div className='absolute bg-white z-50 w-[300px] md:w-[600px] flex flex-col rounded-md shadow-ms p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-      <div className='flex justify-between border-b'>
+      <div className={`absolute bg-white z-50 w-[300px] md:w-[600px] flex flex-col rounded-md shadow-ms p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  ${open ? 'block' : 'hidden'}`}>
+      <div className={`flex justify-between border-b`}>
        <h1 className='text-md text-gray-700 mb-4'>Add Income</h1>
-       <XIcon className='h-5 w-5'/>
+       <XIcon className='h-5 w-5' onClick={() => handleOpen()}/>
       </div>
       <form action="" className='mt-4'>
         <label htmlFor="incomesource">Income Source</label>
@@ -44,7 +63,7 @@ function Income() {
         <button className='w-full py-2 bg-blue-600 text-white font-extrabold cursor-pointer rounded-md mt-4'>Add Income</button>
       </form>
       </div>
-      <div className='absolute bg-black/40 backdrop-blur-sm z-40 w-full h-screen flex flex-col shadow-ms p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
+      <div className={`absolute bg-black/40 backdrop-blur-sm z-40 w-full h-screen  shadow-ms p-4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${open ? 'block transition-all duration-300' : 'hidden'}`}></div>
     </div>
   )
 }
