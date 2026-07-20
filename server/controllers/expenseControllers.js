bf127e3fdf7a5818d722a5f9d615baa5138bdeed
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-20 15:16:22 +0300

    Calculating total income of a single user

--- server/controllers/expenseControllers.js
+++ server/controllers/expenseControllers.js
@@ -19,3 +19,20 @@ export const income = (request,response) => {
         return response.status(500).json({status: false, message: "Internal server error"})
     }
 }
+
+export const totalIncome = (request,response) => {
+    try {
+        const sqlQuerry = "SELECT SUM(incomePrince) as totalIncome FROM income WHERE user_id = ?"
+        database.query(sqlQuerry,[id], (error,result) => {
+            if (error) return response.status(200).json({status: false, message: error})
+            if (result.lengt > 0) {
+                return response.status(200).json({status: true, result: result[0].totalIncome})
+            } else {
+                return response.status(404).json({status: false, message: "user not found"})
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({status: false, message: "Internal server error"})
+    }
+}
