21decba0c47bdde316358aec17aff3011506d5e4
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-21 16:00:16 +0300

    Adding endpoint

--- server/routes/incomeRoutes.js
+++ server/routes/incomeRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { income, totalIncome } from '../controllers/incomeControllers.js'
+import { income, incomeTransaction, totalIncome } from '../controllers/incomeControllers.js'
 
 const incomeRoutes = express.Router()
 incomeRoutes.post('/income',income)
 incomeRoutes.get('/totalIncome/:id',totalIncome)
+incomeRoutes.get('/incomeTransaction/:id',incomeTransaction)
 export default incomeRoutes
