ce06dcd542aafde581671df8b1f5a5e44745a478
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-21 10:53:52 +0300

    Adding expense endpoint

--- server/controllers/expenseControllers.js
+++ server/controllers/expenseControllers.js
@@ -1,4 +1,8 @@
 
 export const expense = (request,response) => {
-    
+    const {userId, expenseDetails, expenseDate, expensePrice} = request.body
+    if (!userId || !expenseDetails || !expenseDate || !expensePrice) {
+        return response.status(400).json({status: false, message: "Please fill all fields"})
+    }
+    console.log(userId,expenseDetails,expenseDate,expensePrice)    
 }
--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { expense } from '../controllers/expenseControllers.js'
 
 const expenseRoutes = express.Router()
+expenseRoutes.post('/expense',expense)
 export default expenseRoutes
