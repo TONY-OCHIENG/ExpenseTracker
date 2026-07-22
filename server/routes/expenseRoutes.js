2ade2ecee24cee23686d42af55ec31a728e85a20
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-07-22 15:37:56 +0300

    Adding endpoints

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { expense, totalExpense } from '../controllers/expenseControllers.js'
+import { expense, expenseDetails, totalExpense } from '../controllers/expenseControllers.js'
 
 const expenseRoutes = express.Router()
 expenseRoutes.post('/expense',expense)
 expenseRoutes.get('/totalExpense/:id',totalExpense)
+expenseRoutes.get('/expenseDetails/:id',expenseDetails)
 export default expenseRoutes
